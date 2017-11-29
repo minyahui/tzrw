049fd763cc3ccdc85eb59659b26801cf85d966d4
MYH <myh@MYHs-Mac-mini.local>
2017-11-29 13:06:57 +0800

    局域网网络对战

---
+++ tzrw/Assets/Script/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+	public GameObject bulletPrefab;//添加public变量bulletPrefab
+	public Transform bulletSpawn;//添加子弹local发射点
+	// Use this for initialization
+
+	[Command]//注意[Command]可以声明一个函数可以本客户端调用，但是会在服务端（主机）执行。
+	void CmdFire() 
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate (
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+		// Spawn the bullet on the Clients
+		NetworkServer.Spawn(bullet);
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
+	}
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z); 
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			print ("发射子弹");
+			CmdFire();      
+		}
+	}
+}
